111b36fed20d8418273e40ee3a93a7c0a43f0715
PentaHIggs <ichiroska89@yahoo.com>
2016-05-22 08:43:09 -0700

    Added functionality to download neccesary caffemodel files

--- settings.py
+++ settings.py
@@ -4,3 +4,8 @@ DEF_PROTOTXT_PATH = "models/VGG_2014_19.prototxt"
 DEF_WEIGHTS_PATH = "models/VGG_2014_19_weights.pyz"
 DEF_BIASES_PATH = "models/VGG_2014_19_biases.pyz"
 
+DEF_CAFFEMODEL_DL =
+"http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel"
+DEF_PROTOTXT_DL =
+"http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel"
+
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import caffe
 import numpy
 import settings as s
-
+from urllib import urlretrieve
 
 def coffeeMachine():
 	coffee = caffe.Net(s.DEF_PROTOTXT_PATH, s.DEF_CAFFEMODEL_PATH, caffe.TEST)
@@ -13,4 +13,10 @@ def coffeeMachine():
 def downloadModel():
 	# Function stub, this function is supposed to download the caffemodel and
 	# prototxt for the vgg19 model
+	def progressBar(blockCount, blockSize, fileSize):
+		# Progress bar for download, passed to urlretrieve
+		return
+	
+	urlretrieve(s.DEF_CAFFEMODEL_DL, s.DEF_CAFFEMODEL_PATH, progressBar)
+	urlretrieve(s.DEF_PROTOTXT_DL, s.DEF_PROTOTXT_PATH, progressBar)
 	return
