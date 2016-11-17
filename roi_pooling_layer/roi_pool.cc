eb298d51d17c0827c582cc9a9712fe9147816fe2
Andy Perez <Ichiroska89@yahoo.com>
2016-11-17 02:06:59 -0500

    Changed code in roi_pool.cc to accept three additional parameters: pooled_height, pooled_width, and feature_stride.  roi_pooling_layer now produces an output of the proper dimensions given it's inputs, and the tests reflect this expectation.

--- roi_pooling_layer/roi_pool.cc
+++ roi_pooling_layer/roi_pool.cc
@@ -1,17 +1,17 @@
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/shape_inference.h"
+#include <vector>
 
 using namespace tensorflow;
 
 REGISTER_OP("RoiPoolingLayer")
-	.Attr("T: numbertype")
+	.Attr("T: numbertype")	
 	.Input("proposal_regions : T")
 	.Input("feature_map: T")
-	.Output("outputs : T")
-	.SetShapeFn([](::tensorflow::shape_inference::InferenceContext *c){
-			c->set_output(0, c->input(1));
-			return Status::OK();
-			});
+	.Attr("pooled_height : int")
+	.Attr("pooled_width : int")
+	.Attr("feature_stride : int")
+	.Output("outputs : T");
 
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/register_types.h"
@@ -19,20 +19,28 @@ REGISTER_OP("RoiPoolingLayer")
 template <typename T>
 class RoiPoolingLayer : public OpKernel {
 	public:
-		explicit RoiPoolingLayer(OpKernelConstruction *context) : OpKernel(context) {}
+		explicit RoiPoolingLayer(OpKernelConstruction *context) : OpKernel(context) 
+		{
+				OP_REQUIRES_OK(context, context->GetAttr("pooled_height", &pooled_height));
+				OP_REQUIRES_OK(context, context->GetAttr("pooled_width", &pooled_width));
+	
+		}
+
 		
 		void Compute(OpKernelContext *context) override {
 				const Tensor& proposal_regions = context->input(0);
 				const Tensor& features = context->input(1);
-				
-				// Creating a garbage output tensor for testing.  Proof of concept,
-				// should return the identity
+							const int roi_dim = proposal_regions.dim_size(0);
+
+				// Creating an empty output with the proper shape
 				Tensor *output_tensor = NULL;
-				OP_REQUIRES_OK(context, context->allocate_output(0, features.shape(),
+				TensorShape output_shape = {roi_dim,pooled_height,pooled_width};
+				OP_REQUIRES_OK(context, context->allocate_output(0, output_shape,\
 																&output_tensor));
-				// Creating a hard copy since context->input(A) objects are const.
-				*output_tensor = context->input(1);	
-				}
+				
+		}
+	private:
+		int feat_h, feat_w, feat_stride, pooled_height, pooled_width;
 };
 
 
--- test/roi_test.py
+++ test/roi_test.py
@@ -11,8 +11,9 @@ class BasicThingTest(tf.test.TestCase):
     def test_output_shape(self):
         regions = tf.constant([[0.,0.,2.,2.,], [4.,6.,10.,10.]])
         feat_map = tf.random_normal((14,14))
-        result = eval_cpu(roi_pooling_layer(regions,feat_map),self)
-        self.assertEqual(result.shape, (14,14))
+        op = roi_pooling_layer(regions, feat_map, 4, 4, 10)
+        result = eval_cpu(op, self)
+        self.assertEqual(result.shape, (2,4,4))
         return
 
 if __name__ == '__main__':
