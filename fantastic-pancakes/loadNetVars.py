ee336a050796006b594bda59d94c44425653ec8f
Andy Perez <ichiroska89@yahoo.com>
2016-11-30 20:29:58 -0500

    Refactored loadNetVars.py to make more sense.  Was going to add to it but realized it was sufficient for my needs so refactored it to be clearer instead

--- fantastic-pancakes/loadNetVars.py
+++ fantastic-pancakes/loadNetVars.py
@@ -2,32 +2,33 @@ import numpy
 import tensorflow as tf
 import util.settings as s
 
+def _fixModelPath(path):
+    # Ensures that sloppily constructed model paths still work
+    if not path.startswits("models/"):
+        path = "models/" + path
+    if not path.endswith(".npz"):
+        return path + ".npz"
+    else:
+        return path
+
 def extractLayers(scope, weightsPath, biasesPath):
-	# Loads parameters from .npz files, one for the weights and another for the biases
-	# Assumes that the proper variable names exist in the given scope somewhere
-	if not weightsPath.startswith("models/"):
-		weightsPath = "models/" + weightsPath
-	if not biasesPath.startswith("models/"):
-		biasesPath = "models/" + biasesPath
-	if not weightsPath.endswith(".npz"):
-		weightsPath = weightsPath + ".npz"
-	if not biasesPath.endswith(".npz"):
-		biasesPath = biasesPath + ".npz"
-	
-	# Raw numpy values.  Need to be loaded into variables.
-	weightsDict = numpy.load(weightsPath)
-	biasesDict = numpy.load(biasesPath)
+    # Loads parameters from .npz files, one for the weights and another for the biases
+    # Assumes that the proper variable names exist in the given scope somewhere
+    weightsPath = _fixModelPath(weightsPath)
+    biasesPath = _fixModelPath(biasesPath)
 
-	# Here, we do a for loop looping through all of the names, "name".
-	for name, weights_tnsr in weightsDict.iteritems():
-		with tf.variable_scope(scope) as model_scope:
-			with tf.variable_scope(name) as layer_scope:		
-				tf.get_variable("Weights", trainable=True, 
-								initializer=tf.constant(weights_tnsr))
-	for name, biases_tnsr in biasesDict.iteritems():
-		with tf.variable_scope(scope) as model_scope:
-			with tf.variable_scope(name) as layer_scope:
-				tf.get_variable("Bias", 	trainable=True, 
-								initializer=tf.constant(biases_tnsr))
+    # Raw numpy values.  Need to be loaded into variables.
+    weightsDict = numpy.load(weightsPath)
+    biasesDict = numpy.load(biasesPath)
 
-	return
+    # Here, we do a for loop looping through all of the names, "name".
+    for name, weights_tnsr in weightsDict.iteritems():
+        with tf.variable_scope(scope) as model_scope:
+            with tf.variable_scope(name) as layer_scope:		
+                tf.get_variable("Weights", trainable=True, initializer=tf.constant(weights_tnsr))
+
+    for name, biases_tnsr in biasesDict.iteritems():
+        with tf.variable_scope(scope) as model_scope:
+            with tf.variable_scope(name) as layer_scope:
+                tf.get_variable("Bias", trainable=True, initializer=tf.constant(biases_tnsr))
+    return
